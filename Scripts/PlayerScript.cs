a99ec80bcc3f8492ad36358eed2d94efdc6e6cac
Krupal Trivedi <kpkuchhadiya@gmail.com>
2023-12-28 14:03:27 +0530

    Animation

---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerScript : MonoBehaviour
+{
+   public static PlayerScript instance;
+   public bool flag;
+   public Animator PlayerAnimation;
+   IEnumerator Coroutine,Coroutine1;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnMouseUp()
+    {
+        GameManager.Instance.FinalWinChecking(this.gameObject);
+        Debug.Log(this.gameObject.name);
+        this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
+
+        flag = true;
+        if (flag == true)
+        {
+            PlayerAnimation.SetBool("walk", true);
+            this.gameObject.transform.DOMove(new Vector3(GameManager.Instance.EmptyObject[GameManager.Instance.Count].transform.position.x, GameManager.Instance.EmptyObject[GameManager.Instance.Count].transform.position.y, GameManager.Instance.EmptyObject[GameManager.Instance.Count].transform.position.z),3);
+            this.gameObject.transform.parent = GameManager.Instance.EmptyObject[GameManager.Instance.Count].transform;
+            GameManager.Instance.Player.Add(this.gameObject);
+            StartCoroutine(GoToHappy(2.5f));
+        }
+       
+        GameManager.Instance.Count++;
+        GameManager.Instance.Checker();
+    }
+
+    public IEnumerator GoToHappy(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        PlayerAnimation.SetBool("walk", false);
+        PlayerAnimation.SetBool("happy", true);
+    }
+    public IEnumerator StopAfterClick(float Delay2)
+    {
+        yield return new WaitForSeconds(Delay2);
+        
+    }
+}
